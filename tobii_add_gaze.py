f36887ec86612ee5ad35b111809760921bef5170
shariqiqbal2810 <shariqiqbal2810@users.noreply.github.com>
2015-11-24 17:16:43 -0500

    Fixed time syncing in tobii_add_gaze.py

--- tobii_add_gaze.py
+++ tobii_add_gaze.py
@@ -6,6 +6,7 @@
 import argparse
 import cv2
 import pandas as pd
+import os
 
 
 def process(gaze_file, infile, outfile):
@@ -13,6 +14,7 @@ def process(gaze_file, infile, outfile):
     # only start tracking eyes once video starts
     gaze = gaze[~gaze.vts_time.isnull()]
 
+    infile = os.path.abspath(os.path.expanduser(infile))
     vid = cv2.VideoCapture(infile)
 
     size = (1920, 1080)
@@ -38,21 +40,34 @@ def process(gaze_file, infile, outfile):
         prog_ratio = (frame_num * 1.0 / tot_frames)
         print ('[' + int(prog_ratio * 50) * '=' + int((1 - prog_ratio) * 50) * '-' + ']'
                ' %.1f %% Complete\r' % (prog_ratio * 100)),
-
         ret, frame = vid.read()
         if ret:
-            while vts[i] < vid_time:
-                frame_cp = frame.copy()
-                if gaze_val[i] == 0:
-                    gp_x = gaze_x[i]
-                    gp_y = gaze_y[i]
-                    cv2.circle(
-                        frame_cp, (int(1920 * gp_x), int(1080 * gp_y)),
-                        8, [255, 0, 0], -2)
-                out.write(frame_cp)
-                i += 1
+            # make sure two frames are written per one original frame (to
+            # maintain playback speed)
+            for j in range(2):
+                # make sure eye tracking data exists for current frame
+                if i < len(vts):
+                    # make sure eye tracking data is not ahead of video
+                    if vts[i] < vid_time:
+                        frame_cp = frame.copy()
+                        if gaze_val[i] == 0:
+                            gp_x = gaze_x[i]
+                            gp_y = gaze_y[i]
+                            cv2.circle(
+                                frame_cp, (int(1920 * gp_x), int(1080 * gp_y)),
+                                8, [255, 0, 0], -2)
+                        out.write(frame_cp)
+                        i += 1
+                    else:
+                        out.write(frame)
+                else:
+                    out.write(frame)
         else:
             break
+        # make sure eye tracking data is caught up with video
+        if i + 1 < len(vts):
+            while vts[i + 1] < vid_time:
+                i += 1
 
     print
     vid.release()
