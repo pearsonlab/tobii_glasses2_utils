42e3170971a6a4124e21a4059d963dd85902956a
shariqiqbal2810 <shariqiqbal2810@users.noreply.github.com>
2016-03-14 13:45:39 -0400

    Automatic IP detection in tobii_record

--- tobii_record.py
+++ tobii_record.py
@@ -12,7 +12,7 @@ import time
 import socket
 import sys
 
-GLASSES_IP = "192.168.71.50"  # IPv4 address
+GLASSES_IP = socket.gethostbyname(socket.gethostname())  # IPv4 address
 PORT = 49152
 base_url = 'http://' + GLASSES_IP
 timeout = 1
